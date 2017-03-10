f068ebff5d1e3ebf8acff88ea45b76c359ff12e9
Matthew Comee <MComee87@users.noreply.github.com>
2017-03-10 19:57:11 +0000

    Add files via upload
    add create new menu item

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -20,22 +20,22 @@ def restaurantMenu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items) 
 
 # Task 1: Create route for newMenuItem function here
-
-
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
-
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'],restaurant_id  = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+    
 # Task 2: Create route for editMenuItem function here
-
-
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
-
-
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
